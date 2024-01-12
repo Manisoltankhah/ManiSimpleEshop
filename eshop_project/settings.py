89fd7ce29ab56f92ec0e3f31ce1e4e0948ea6338
Manisoltankhah <m.s.1384.mani@gmail.com>
2024-01-12 15:28:27 +0330

    forth commit

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -158,8 +158,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'manisoltan325@gmail.com'
-EMAIL_HOST_PASSWORD = '1384m1384_Ms'
+EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_PORT = 587
 
 # default settings
